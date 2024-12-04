fa59f1260e973dd312a49d64cd759b67c2c5bdc3
huuloc214 <aikido2142005@gmail.com>
2024-12-04 19:53:22 +0700

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -119,6 +119,15 @@ router.get("/find_by_videoID", async function (req, res) {
   }
 });
 
+router.get("/get_ all_student_majo", async function (req, res) {
+  try {
+    const { m } = req.query;
+    var list = await studentModel.find({ BM: { $eq: m } });
+    res.status(200).json(list);
+  } catch (error) {
+    res.status(404).json({ statas: false, message: " có lỗi xãy ra" + error });
+  }
+});
 
 
 module.exports = router;
